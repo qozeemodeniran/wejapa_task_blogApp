0c22ce11700c26edea0e97782149362f52104e4f
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-09-12 17:31:13 +0100

    started forgot password feature

--- login.php
+++ login.php
@@ -17,6 +17,8 @@
 			<input type="text" name="username" value="<?php echo $username; ?>" value="" placeholder="Username">
 			<input type="password" name="password" placeholder="Password">
 			<button type="submit" class="btn" name="login_btn">Login</button>
+			<button type="submit" class="btn" name="login_btn">Forgot Password?</button>
+
 			<p>
 				Not yet a member? <a href="register.php">Sign up</a>
 			</p>
